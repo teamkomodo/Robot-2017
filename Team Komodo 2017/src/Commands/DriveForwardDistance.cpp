295c1552d29c58a98e07f79fda8dd882100fa516
MEDavy <maximiliandavy@gmail.com>
2017-03-14 18:45:30 -0600

    Autonomous Changes
    Fix gyro drive and add reverse afterward

--- Team Komodo 2017/src/Commands/DriveForwardDistance.cpp
+++ Team Komodo 2017/src/Commands/DriveForwardDistance.cpp
@@ -5,12 +5,16 @@
 #include "DriveForwardDistance.h"
 #include "OI.h"
 #include "EncoderConverter.h"
+#include "WPIlib.h"
 
 DriveForwardDistance::DriveForwardDistance() : CommandBase("DriveForwardDistance") {
 	// Use Requires() here to declare subsystem dependencies
 	// eg. Requires(Robot::chassis.get());
 	Requires(CommandBase::retrieveDriveSubsystem());
-	distanceInches = 66;//7ft 10in (94 in)from alliance station to baseline - 28 inches robot length
+	distanceInches = 150; // was 100
+	reverseDistanceInches = 20;
+	autoCount = 1;
+	waitTime = 3; //was 3
 	driveSubsystem = CommandBase::retrieveDriveSubsystem();
 	driveGyro = driveSubsystem->GetDriveGyro();
 	editedGyroAngle = 0;
@@ -20,32 +24,63 @@ DriveForwardDistance::DriveForwardDistance() : CommandBase("DriveForwardDistance
 void DriveForwardDistance::Initialize() {
 	driveSubsystem->ResetLeftEncoder();
 	driveSubsystem->ResetRightEncoder();
-	distanceInches = SmartDashboard::GetNumber("autonomousDistance", 66);
+	driveGyro->Reset();
+	//distanceInches = SmartDashboard::GetNumber("autonomousDistance", 66);
 }
 
 // Called repeatedly when this Command is scheduled to run
 void DriveForwardDistance::Execute() {
-	if (fabs(driveGyro->GetRate()) > 0.035 && fabs(driveGyro->GetRate()) < 30) {
+	if (fabs(driveGyro->GetRate()) > 0.04 ) {	//works well at .04
 		editedGyroAngle = driveGyro->GetAngle();
 	}else{
 		driveGyro->Reset();
 		editedGyroAngle = 0;
 	}
+	SmartDashboard::PutNumber("Auto count", autoCount);
+	SmartDashboard::PutNumber("Gyro Angle", driveGyro->GetAngle());
+	SmartDashboard::PutNumber("Gyro Rate", driveGyro->GetRate());
 	SmartDashboard::PutNumber("Left Encoder Value", driveSubsystem->GetLeftEncoderValue());
 	SmartDashboard::PutNumber("Right Encoder Value", driveSubsystem->GetRightEncoderValue());
 	//if (fabs(driveSubsystem->GetLeftEncoderValue()) < EncoderConverter::InchesToEncoder(distanceInches)
 	//  && fabs(driveSubsystem->GetRightEncoderValue()) < EncoderConverter::InchesToEncoder(distanceInches)){
-	if (fabs(driveSubsystem->GetLeftEncoderValue()) < distanceInches * ENCODER_VALUES_PER_INCH
-		&& fabs(driveSubsystem->GetRightEncoderValue()) < distanceInches * ENCODER_VALUES_PER_INCH){
-			driveSubsystem->Arcade(.5, 0, editedGyroAngle);
+if (autoCount == 1){
+
+	if (fabs(driveSubsystem->GetLeftEncoderValue()*-1) < distanceInches * ENCODER_VALUES_PER_INCH
+		|| fabs(driveSubsystem->GetRightEncoderValue()*-1) < distanceInches * ENCODER_VALUES_PER_INCH){
+			driveSubsystem->Arcade(-.75, 0.0, editedGyroAngle);	//was .75
 		} else {
 			driveSubsystem->ResetRightEncoder();
 			driveSubsystem->ResetLeftEncoder();
+			driveGyro->Reset();
 			driveSubsystem->Arcade(0,0, 0);
+			//Wait(waitTime);
+			driveSubsystem->ResetRightEncoder();
+			driveSubsystem->ResetLeftEncoder();
+			//autoCount = 2;
 			isDone = true;
-		}
 
+		}
 }
+
+else if (autoCount == 2){
+	if(fabs(driveSubsystem->GetLeftEncoderValue()) < reverseDistanceInches * ENCODER_VALUES_PER_INCH
+		|| fabs(driveSubsystem->GetRightEncoderValue()) < reverseDistanceInches * ENCODER_VALUES_PER_INCH){
+				driveSubsystem->Arcade(.8, 0.0, editedGyroAngle);	//was .75
+			}
+	else {
+				driveSubsystem->ResetRightEncoder();
+				driveSubsystem->ResetLeftEncoder();
+				driveSubsystem->Arcade(0,0, 0);
+				//Wait(20);
+				isDone = true;
+
+			}
+			}
+			}
+
+
+
+
 
 // Make this return true when this Command no longer needs to run execute()
 bool DriveForwardDistance::IsFinished() {
